94c6f0c59be437a42780f404e5927a67727f1633
unknown <debbyria30@gmail.com>
2022-05-20 01:53:56 +0700

    setup expo, navigation, icon, and safeareview

--- m-cure-mobile/App.js
+++ m-cure-mobile/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { GestureHandlerRootView, GestureDetector, Gesture } from 'react-native-gesture-handler';
 
+const Stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <GestureHandlerRootView>
+        <SafeAreaView style={{ flex: 1 }}>
+          <GestureDetector>
+            <Stack.Navigator>
+            //pages
+            </Stack.Navigator>
+          </GestureDetector>
+        </SafeAreaView>
+      </GestureHandlerRootView>
+    </NavigationContainer>
   );
 }
 
---
+++ m-cure-mobile/src/colors.js
@@ -0,0 +1,16 @@
+const COLORS = {
+  white: '#fff',
+  mainGreen: '#B4E197',
+  textGreen: '#4E944F',
+  textBlue: '#235BED',
+  textRed: '#F43F3F',
+  buttonGreen: '#E9EFC0',
+  buttonRed: '#EA1616',
+  fillColor: '#F3F3F3',
+  chatGreen: '#5FE82F',
+  chatGray: '#CFD7CC',
+  textGray: '#BBBBBB',
+  textProfile: '#6A6A6A',
+}
+
+export default COLORS;
