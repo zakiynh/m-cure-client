86778aa9b603d85811c0774a14df470655e1568b
M Zakiy Nurhasyim <zakiynurhasyim@gmail.com>
2022-05-21 11:35:12 +0700

    consultation history layout 95%, not done yet

--- m-cure-mobile/screen/ConsultationHistory.js
+++ m-cure-mobile/screen/ConsultationHistory.js
@@ -4,22 +4,24 @@ import COLORS from "../src/colors";
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import { useTailwind } from "tailwind-rn";
 
 const image = { uri: "https://th.bing.com/th/id/OIP.uJg0Ku4GimXqktPdSC3YAgHaJT?pid=ImgDet&w=860&h=1081&rs=1" };
 const data = [
-    { image, name: "Consultant A", history: "terimakasih" },
-    { image, name: "Consultant B", history: "oke" },
-    { image, name: "Consultant C", history: "sama-sama" },
-    { image, name: "Consultant D", history: "uhuy" },
-    { image, name: "Consultant E", history: "ini" },
-    { image, name: "Consultant F", history: "history" },
-    { image, name: "Consultant G", history: "chat" },
-    { image, name: "Consultant H", history: "sekian" },
-    { image, name: "Consultant I", history: "dan" },
-    { image, name: "Consultant J", history: "terimakasih" },
+    { image, name: "Consultant A", history: "terimakasih", date: "13/06/2022" },
+    { image, name: "Consultant B", history: "oke", date: "13/06/2021" },
+    { image, name: "Consultant C", history: "sama-sama", date: "13/02/2022" },
+    { image, name: "Consultant D", history: "uhuy", date: "13/01/2022" },
+    { image, name: "Consultant E", history: "ini", date: "13/01/2022" },
+    { image, name: "Consultant F", history: "history", date: "13/02/2022" },
+    { image, name: "Consultant G", history: "chat", date: "13/02/2022" },
+    { image, name: "Consultant H", history: "sekian", date: "13/01/2022" },
+    { image, name: "Consultant I", history: "dan", date: "13/02/2022" },
+    { image, name: "Consultant J", history: "terimakasih", date: "13/04/2022" },
 ];
 
 export default function ConsultantList() {
+    const tailwind = useTailwind();
     return (
         <View style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
@@ -35,8 +37,8 @@ export default function ConsultantList() {
                             <View style={styles.consultant}>
                                 <View style={{ flexDirection: "row", marginTop: 15 }}>
                                     <Avatar.Image source={item.image} size={80} />
-                                    <View style={{ flexDirection: "row", marginLeft: 20 }}>
-                                        <View style={{ marginLeft: 20 }}>
+                                    <View style={{ flexDirection: "row", justifyContent:"space-between", marginLeft: 20 }}>
+                                        <View style={{ marginLeft: 10, marginRight: 10 }}>
                                             <Title
                                                 style={[
                                                     styles.consultantName,
@@ -47,6 +49,7 @@ export default function ConsultantList() {
                                                 ]}
                                             >
                                                 {item.name}
+                                            <View><Caption style={[styles.chat, {alignItems: 'flex-end'}]}>{item.date}</Caption></View>
                                             </Title>
                                             <Caption style={styles.chat}>{item.history}</Caption>
                                         </View>
