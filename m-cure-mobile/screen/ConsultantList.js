5d392ee03b76630470ae72eff00f328aa0b303f3
M Zakiy Nurhasyim <zakiynurhasyim@gmail.com>
2022-05-21 12:57:43 +0700

    get data from server

--- m-cure-mobile/screen/ConsultantList.js
+++ m-cure-mobile/screen/ConsultantList.js
@@ -1,25 +1,47 @@
+import { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { Avatar, Title } from "react-native-paper";
 import COLORS from "../src/colors";
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import axios from 'axios';
 
 const image = { uri: "https://th.bing.com/th/id/OIP.uJg0Ku4GimXqktPdSC3YAgHaJT?pid=ImgDet&w=860&h=1081&rs=1" };
-const data = [
-    {image, name: "Consultant A"},
-    {image, name: "Consultant B"},
-    {image, name: "Consultant C"},
-    {image, name: "Consultant D"},
-    {image, name: "Consultant E"},
-    {image, name: "Consultant F"},
-    {image, name: "Consultant G"},
-    {image, name: "Consultant H"},
-    {image, name: "Consultant I"},
-    {image, name: "Consultant J"},
-]
+// const data = [
+//     {image, name: "Consultant A"},
+//     {image, name: "Consultant B"},
+//     {image, name: "Consultant C"},
+//     {image, name: "Consultant D"},
+//     {image, name: "Consultant E"},
+//     {image, name: "Consultant F"},
+//     {image, name: "Consultant G"},
+//     {image, name: "Consultant H"},
+//     {image, name: "Consultant I"},
+//     {image, name: "Consultant J"},
+// ]
+
+const baseUrl = "https://m-cure-origin.herokuapp.com/"
 
 export default function ConsultantList() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios( baseUrl + "users/consultants", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJyYXRpaHNhbmpheWFAbWFpbC5jb20iLCJpYXQiOjE2NTMxMTIyOTUsImV4cCI6MTY1MzExOTQ5NX0.bTJmy7nH6iQ2wPJe3H7wiWobXLvzFgEHXilqHuwJ0vk"
+            }
+        })
+        .then(res => {
+            const data = res.data
+            // console.log("data: ", data);
+            setData(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
     return (
         <View style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
@@ -46,9 +68,9 @@ export default function ConsultantList() {
                                         >
                                             {item.name}
                                         </Title>
-                                        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+                                        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
                                             <Entypo style={styles.logo} name="chat" size={30} color={COLORS.mainGreen} />
-                                            <FontAwesome style={styles.logo} name="video-camera" size={30} color={COLORS.mainGreen} />
+                                            <FontAwesome style={[styles.logo, {marginLeft: 40}]} name="video-camera" size={30} color={COLORS.mainGreen} />
                                         </View>
                                     </View>
                                 </View>
