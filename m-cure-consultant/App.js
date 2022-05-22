4e7bff5d5e27d30d2707a3e402f25080a61f1cbd
M Zakiy Nurhasyim <zakiynurhasyim@gmail.com>
2022-05-23 02:05:10 +0700

    first setup

---
+++ m-cure-consultant/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { GestureHandlerRootView, GestureDetector, Gesture } from 'react-native-gesture-handler';
+import MainStack from './navigation/MainStack';
+
+import { TailwindProvider } from 'tailwind-rn';
+import utilities from './tailwind.json';
+
+export default function App() {
+  return (
+    <TailwindProvider utilities={utilities}>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </TailwindProvider>
+
+  );
+}
