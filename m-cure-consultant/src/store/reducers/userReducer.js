7b68a7a18672dd02d62f34a0c3ec4cc1e1d24e5b
M Zakiy Nurhasyim <zakiynurhasyim@gmail.com>
2022-05-25 00:58:08 +0700

    video code and video call

--- m-cure-consultant/src/store/reducers/userReducer.js
+++ m-cure-consultant/src/store/reducers/userReducer.js
@@ -1,9 +1,12 @@
-import { LOGIN_USER_SUCCESS } from "../actions/actionTypes";
+import { LOGIN_USER_SUCCESS, VIDEO_CODE, STATUS } from "../actions/actionTypes";
 
 const initialState = {
   email: "",
   username: "",
   access_token: "",
+  videoCode: "",
+  status: "",
+  id: ""
 }
 
 function userReducer(state = initialState, action) {
@@ -11,8 +14,20 @@ function userReducer(state = initialState, action) {
     case LOGIN_USER_SUCCESS:
       return {
         ...state,
-        email: action.payload.email,
-        access_token: action.payload.access_token
+        email: action.payload.user.email,
+        access_token: action.payload.access_token,
+        id: action.payload.user.id,
+        status: action.payload.user.status
+      }
+    case VIDEO_CODE:
+      return {
+        ...state,
+        videoCode: action.payload.videoCode
+      }
+    case STATUS:
+      return {
+        ...state,
+        status: action.payload.status
       }
     default:
       return state
